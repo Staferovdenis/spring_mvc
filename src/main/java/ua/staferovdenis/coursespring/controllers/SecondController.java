0cba15afa3e85a6d4dd547cdce206ef6efd59e06
Staferovdenis <staferovdenis7@gmail.com>
2020-06-18 13:08:51 +0300

    Java_Controller

---
+++ src/main/java/ua/staferovdenis/coursespring/controllers/SecondController.java
@@ -0,0 +1,12 @@
+package ua.staferovdenis.coursespring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+    @GetMapping("/exit")
+    public String exit(){
+        return "second/exit";
+    }
+}
